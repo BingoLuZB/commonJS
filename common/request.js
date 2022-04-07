51589831df3cc39af9887cc81ceed3c031a0157a
BingoLuZB <38916948+BingoLuZB@users.noreply.github.com>
2022-04-07 11:59:37 +0800

    Update request.js

--- common/request.js
+++ common/request.js
@@ -10,7 +10,7 @@ axios.defaults.baseURL = config.baseURL
 // 请求超时时间
 axios.defaults.timeout = 10000;
 // 解决跨域
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 // post请求头
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 
